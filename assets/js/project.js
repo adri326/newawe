23569138210c6ee021b2e29cd66e8d027cbdece6
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-03-30 14:35:41 +1300

    Yay

---
+++ assets/js/project.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+ * Represents an editor for a file
+ * in a project.
+ */
+function Editor(filename) {
+    this.filename = filename;
+    this.element = document.createElement('textarea');
+    this.element.placeholder = 'textareas suck';
+    this.element.className = 'editor';
+    this.element.id = 'editor-for-' + filename;
+}
+
+/*
+ * Represents a single file in a
+ * project.
+ */
+function File(name) {
+    var title = document.createElement('h4');
+    var editor = new Editor(name);
+    title.textContent = name;
+    this.element = document.createElement('li');
+    this.element.appendChild(title);
+    this.element.appendChild(editor.element);
+}
+
+/*
+ * Represents a project.
+ */
+function Project(name) {
+    var title = document.querySelector('h3#project-name');
+    title.textContent = name;
+    this.name = name;
+    this.files = {};
+}
+
+Project.prototype.addfile = function() {
+    var name = prompt('File name?');
+    var files = document.querySelector('ul#files');
+    var file = new File(name);
+    files.appendChild(file.element);
+    this.files[name] = file;
+}
